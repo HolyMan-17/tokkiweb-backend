a2357fc01128e731ca9d981d41a0365da249cb36
HolyMan_17 <85586852+HolyMan-17@users.noreply.github.com>
2026-07-04 12:43:58 -0400

    Routing for products setup

---
+++ src/routes/products.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { productsController } from '../controllers/c_products.js'
+
+const router = Router();
+
+router.get('/', productsController.getAllProducts);
+router.get('/:product_id', productsController.getProduct);
+router.post('/', productsController.createProduct);
+router.patch('/:product_id', productsController.updateProductDetails);
+router.delete('/:product_id', productsController.deleteProduct);
+
+export default router
